27d8da9cf7a5c6c4324fd7b78dd84b8c20fbc0c4
Dan McElroy <daniel@mcelroys.net>
2023-11-20 18:47:46 +0100

    Updated schema

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -1,38 +1,48 @@
 CREATE EXTENSION if not exists "uuid-ossp";
 
 CREATE TABLE if not exists artist (
-    id uuid default uuid_generate_v4() PRIMARY KEY,
-    version serial,
-    creation_time timestamp without time zone,
+    id uuid default uuid_generate_v4() primary key,
+    version integer default 1,
+    creation_time timestamp without time zone default now(),
     name varchar(255),
     profile_picture_url varchar(255)
 );
 
 CREATE TABLE if not exists artist_alias (
-    id uuid default uuid_generate_v4() PRIMARY KEY,
+    id uuid default uuid_generate_v4() primary key,
     alias varchar(255),
-    artist_id uuid REFERENCES artist (id) ON DELETE CASCADE
+    artist_id uuid not null,
+    foreign key (artist_id) references artist (id)
+        match simple on update no action on delete cascade
 );
 
 CREATE TABLE if not exists genre (
-    id uuid default uuid_generate_v4() PRIMARY KEY,
-    version serial,
-    creation_time timestamp without time zone,
+    id uuid default uuid_generate_v4() primary key,
+    version integer default 1,
+    creation_time timestamp without time zone default now(),
     name varchar(255)
 );
 
 CREATE TABLE if not exists track (
-    id uuid default uuid_generate_v4() PRIMARY KEY,
-    version serial,
-    creation_time timestamp without time zone,
+    id uuid default uuid_generate_v4() primary key,
+    version integer default 1,
+    creation_time timestamp without time zone default now(),
     title varchar(255),
-    genre_id uuid REFERENCES genre (id) ON UPDATE CASCADE,
-    length_seconds serial CHECK (length_seconds > 0)
+    genre_id uuid,
+    length_seconds integer check (length_seconds > 0),
+    foreign key (genre_id) references genre (id)
+        match simple on update no action on delete no action
 );
 
 CREATE TABLE if not exists artist_tracks (
-    track_id uuid REFERENCES track (id) ON UPDATE CASCADE,
-    artist_id uuid REFERENCES artist (id) ON UPDATE CASCADE
+    artist_id uuid not null,
+    track_id uuid not null,
+    primary key (artist_id, track_id),
+    foreign key (artist_id) references artist (id)
+        match simple on update no action on delete no action,
+    foreign key (track_id) references track (id)
+        match simple on update no action on delete no action
 );
 
-CREATE INDEX creation_time_idx ON artist (creation_time)
+CREATE INDEX creation_time_idx ON artist using btree (creation_time);
+
